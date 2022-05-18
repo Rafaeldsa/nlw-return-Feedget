180fa5fe50b88beef0bf9622f27adb4e7fd70e22
Rafaeldsa <rafael.azevedo@virtus.ufcg.edu.br>
2022-05-18 09:06:04 -0300

    send mail done

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
 import { prisma } from "./prisma";
+import nodemailer from 'nodemailer';
 import express from "express";
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "bb7ef328b55d71",
+    pass: "76021a61cf50d4"
+  }
+})
 
 app.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -14,6 +24,18 @@ app.post("/feedbacks", async (req, res) => {
       comment,
       screenshot,
     },
+  });
+
+  transport.sendMail({
+    from: "Equipe Feedget <oi@feedget.com>",
+    to: "Rafael Dantas <rafaeldantas461@gmail.com",
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n')
   });
 
   return res.status(201).json({ data: feedback });
